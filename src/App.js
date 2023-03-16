988f2c4397b03791e442c62cb500637b0cf180c3
Md Asif Raza <itsmdasifraza@gmail.com>
2023-03-16 14:43:07 +0530

    Added router component to app (#8)
    Signed-off-by: Md Asif Raza <itsmdasifraza@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { Route, Routes } from "react-router-dom"
 import './App.css';
 import Home from "./components/pages/home/Home.js";
 import Login from "./components/pages/login/Login.js";
 function App() {
   return (
     <>
-    <Home/>
-    <Login/>
+     <Routes>
+      <Route  path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
     </>
   );
 }
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom"
 import Button from '@mui/material/Button';
 import "./Home.css";
 import landingPic from '../../../assets/home-landing-page.png';
@@ -11,8 +12,8 @@ const Home = () => {
           <div className="col-12">
             <h1 className="pt-3 pb-2">eStore</h1>
             <p>Your First Shop built on HTML, CSS, AND, JavaScript</p>
-            <a href="/login"><Button variant="contained" size="medium">Login</Button></a>
-            <a href="/signup"><Button variant="contained" size="medium">Signup</Button></a>
+            <Link to="/login"><Button variant="contained" size="medium">Login</Button></Link>
+            <Link to="/register"><Button variant="contained" size="medium">Register</Button></Link>
             <br/>
             <div style={{display:"flex", flexStart:"end"}}>
               <img src={landingPic} width="40%" style={{marginRight: 0, marginLeft: "auto"}} alt=""/>
