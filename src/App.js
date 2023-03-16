0a76e65fe0ab895c4f00c0addec3ad8deaacf443
Md Asif Raza <itsmdasifraza@gmail.com>
2023-03-16 13:39:18 +0530

    Added login page to the app (#1)
    Signed-off-by: Md Asif Raza <itsmdasifraza@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Home from "./components/pages/home/Home.js";
-
+import Login from "./components/pages/login/Login.js";
 function App() {
   return (
     <>
     <Home/>
+    <Login/>
     </>
   );
 }
