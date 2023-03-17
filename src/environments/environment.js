ff32b4525490539e43c7c93b1854b926953779df
Md Asif Raza <itsmdasifraza@gmail.com>
2023-03-17 19:52:06 +0530

    Added environment file to store app data (#16)
    Signed-off-by: Md Asif Raza <itsmdasifraza@gmail.com>

---
+++ src/environments/environment.js
@@ -0,0 +1,15 @@
+export const environment = {
+    backend: {
+      connection : "http",
+      ip : "127.0.0.1:3000"
+    },
+    app : {
+      name : "eStore",
+    },
+    developer : {
+      name : "Md Asif Raza",
+      position : "Software Developer",
+      email : "itsmdasifraza@gmail.com"
+    }
+  };
+  
