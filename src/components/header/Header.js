05948e696097977e00bdce453026a2fe27fab1a4
Md Asif Raza <itsmdasifraza@gmail.com>
2023-03-23 16:28:53 +0530

    Upgraded dashboard button with dropdown menu of mui (#46)
    Signed-off-by: Md Asif Raza <itsmdasifraza@gmail.com>

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,10 +2,28 @@ import React from "react";
 import "./Header.css";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom"
-import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+
 const Header = () => {
     const login = useSelector((state) => state.login);
     const navigate = useNavigate();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
     return (
         <>
             <section className= "header">
@@ -19,11 +37,86 @@ const Header = () => {
                     {!login ? <Link to="/register">Register</Link> : <></>}
                     {login ? <Link to="/shop">Shop</Link> : <></>}
                     {login ? <Link to="/cart">Cart</Link> : <></>}
-                    {login ? <Link to="/profile">Profile</Link> : <></>}
-                    {login ? <Button onClick={()=>{
+                    
+                    
+
+                    {login ? <React.Fragment>
+      {/* <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}> */}
+        {/* <Tooltip title="Account settings"> */}
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <Avatar sx={{ width: 32, height: 32, backgroundColor: "#0080ff" }}>{ JSON.parse(localStorage.getItem("currentUser")).name[0].toUpperCase()}</Avatar>
+          </IconButton>
+        {/* </Tooltip> */}
+      {/* </Box> */}
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <Link to="/profile" ><MenuItem sx={{color:"black"}} onClick={handleClose}>
+        <Avatar sx={{ width: 32, height: 32 , backgroundColor: "#0080ff" }}>{ JSON.parse(localStorage.getItem("currentUser")).name[0].toUpperCase()}</Avatar> My profile 
+        </MenuItem></Link>
+        <Divider />
+        <Link to="/password" >
+        <MenuItem sx={{color:"black"}} onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Change password
+        </MenuItem></Link>
+        
+        <MenuItem onClick={()=>{
                         localStorage.removeItem("currentUser");
                         navigate('/login');
-                    }} variant="contained" size="medium">Logout</Button> : <></>}
+                    }} sx={{color:"black"}} 
+                    // onClick={handleClose}
+                    >
+          <ListItemIcon>
+            <Logout  fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </React.Fragment> : <></>}
+                    
+                    
                     </div>
                 </nav>
             </section>
