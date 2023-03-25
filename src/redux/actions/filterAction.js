a823aa77a30202f0ff2141e37ce5a78579aa89ae
Md Asif Raza <itsmdasifraza@gmail.com>
2023-03-25 12:35:12 +0530

    Setup redux for storing current filtering options (#68)
    Signed-off-by: Md Asif Raza <itsmdasifraza@gmail.com>

---
+++ src/redux/actions/filterAction.js
@@ -0,0 +1,22 @@
+const setRate = (rate) => {
+    return {
+      type: "SET_RATE",
+      payload: rate,
+    };
+};
+
+const setCategory = (category) => {
+    return {
+      type: "SET_CATEGORY",
+      payload: category,
+    };
+};
+
+const setSearchText = (text) => {
+    return {
+      type: "SET_SEARCH_TEXT",
+      payload: text,
+    };
+};
+
+export {setRate, setCategory, setSearchText};
---
+++ src/redux/reducers/filterReducer.js
@@ -0,0 +1,23 @@
+const intialState = {
+    rate: 0,
+    categoryStatus: "all",
+    searchText: ""
+};
+
+const filterReducer = (state = intialState, { type, payload }) => {
+  switch (type) {
+    case "SET_RATE":
+        const s1 = {...state, rate: payload};
+      return s1;
+    case "SET_CATEGORY":
+        const s2 = {...state, categoryStatus: payload};
+        return s2;
+    case "SET_SEARCH_TEXT":
+        const s3 = {...state, searchText: payload};
+        return s3;
+    default:
+      return state;
+  }
+};
+
+export default filterReducer;
